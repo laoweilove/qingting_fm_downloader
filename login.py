b6408b0df5628706b3b30dcca8f4e7b54c9e8eeb
laoweilove <54768931+laoweilove@users.noreply.github.com>
2022-11-03 09:22:39 +0800

    Update login.py

--- login.py
+++ login.py
@@ -6,9 +6,10 @@ h = {
     'Content-Type': 'application/json',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.124 Safari/537.36 Edg/102.0.1245.44',
 }
-
-user_id = input('手机号')
-password = input('密码')
+config_file = open('config.yaml')
+config = yaml.load(config_file, Loader=yaml.Loader)
+user_id = config['phone']
+password = config['password']
 
 
 d = {"account_type":"5","device_id":"web","user_id":user_id,"password":password,"area_code":"+86"}
@@ -22,5 +23,8 @@ data = {
     'cookie': cookie,
     'access_token': access_token,
     'qingting_id': qingting_id,
+    'phone': user_id,
+    'password': password
+
 }
 yaml.dump(data,w)
